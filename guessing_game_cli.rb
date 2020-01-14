d7fdc561f54958cc6a32e9cc751452d0ba5f4000
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-14 03:22:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -22,7 +22,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif users_guess == "exit"
     puts "Goodbye!"
-  else
+  elsif users_guess != hidden_number && users_guess != "exit"
     puts "Sorry! The computer guessed #{hidden_number}"
   end
 end
