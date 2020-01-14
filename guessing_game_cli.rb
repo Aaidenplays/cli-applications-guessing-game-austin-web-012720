e1eb1ccc4d02f2a52039b7b3cc9ff17890a878a9
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-14 03:36:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -16,7 +16,7 @@ def run_guessing_game
   hidden_number = generate_random_number
   prompt_user
   users_guess = user_input
-  binding.pry
+ # binding.pry
   if users_guess == hidden_number
     puts "You guessed the correct number!"
   elsif users_guess == "exit"
