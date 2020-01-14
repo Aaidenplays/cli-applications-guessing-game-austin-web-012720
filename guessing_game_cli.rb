2f80d1aceb39b26a463c202ea0caa8ed39d443b4
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-14 03:17:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,24 @@
 # Code your solution here!
 
+def generate_random_number
+  rand(1..6)
+end
+
+def prompt_user
+  puts "Please enter a number between 1 and 6"
+end
+
+def user_input
+  gets
+end
 
 
 def run_guessing_game
-
+  hidden_number = generate_random_number
+  prompt_user
+  users_guess = user_input
+  
+  if users_guess == hidden_number
+    puts "You guessed the correct number!"
+  end
 end
